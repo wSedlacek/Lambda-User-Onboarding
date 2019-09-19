aa9be50a84660ca73e58a41241c05c5686e32b79
William Sedlacek <wsedlacekc@gmail.com>
2019-09-19 16:13:56 -0500

    Style Card

--- src/app/components/card/card.component.tsx
+++ src/app/components/card/card.component.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import './card.component.scss';
 
+import { default as MatCard } from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+
 import { User } from '../../models/User';
 
 export const Card = ({ user }: { user: User }) => {
-  return <div className='Card'>{user.name}</div>;
+  const { name, role, email } = user;
+  return (
+    <MatCard className='Card'>
+      <Typography variant='h5' component='h2'>
+        {name}
+      </Typography>
+      <Typography gutterBottom variant='subtitle1' component='h3'>
+        {role}
+      </Typography>
+      <Typography variant='subtitle2' component='h4'>
+        {email}
+      </Typography>
+    </MatCard>
+  );
 };
