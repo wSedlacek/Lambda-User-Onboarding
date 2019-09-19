86a402b15ed1017bb1f91f9657accbb2f0653d36
William Sedlacek <wsedlacekc@gmail.com>
2019-09-19 16:04:44 -0500

    Style Form

--- src/app/components/form/form.component.tsx
+++ src/app/components/form/form.component.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import './form.component.scss';
 
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
 import { withFormik, Form as Formik, Field } from 'formik';
+import { TextField, Checkbox } from 'formik-material-ui';
 import * as Yup from 'yup';
 
 import { User } from '../../models/User';
@@ -9,36 +17,47 @@ import { UserService } from '../../services/user.service';
 
 type SignupFormProps = {
   values: User;
-  errors: { name: string; email: string; password: string };
+  errors: { name: string; email: string; password: string; tos: string };
   touched: { name: boolean; email: boolean; password: boolean; tos: boolean };
   isSubmitting: boolean;
 };
 
 function SignupForm({ values, errors, touched, isSubmitting }: SignupFormProps) {
   return (
-    <Formik>
-      <div>
-        {touched.name && errors.name && <p>{errors.name}</p>}
-        <Field type='name' name='name' placeholder='Name' />
-      </div>
+    <Paper className='Form'>
+      <Typography gutterBottom variant='h4' component='h2'>
+        Signup
+      </Typography>
+      <Formik className='Inputs'>
+        <Field className='Input' type='name' name='name' placeholder='Name' component={TextField} />
+        <Field
+          className='Input'
+          type='email'
+          name='email'
+          placeholder='Email'
+          component={TextField}
+        />
+        <Field
+          className='Input'
+          type='password'
+          name='password'
+          placeholder='Password'
+          component={TextField}
+        />
 
-      <div>
-        {touched.email && errors.email && <p>{errors.email}</p>}
-        <Field type='email' name='email' placeholder='Email' />
-      </div>
+        <FormControl className='TOS' error={errors.tos !== undefined && touched.tos}>
+          <FormLabel component='legend'>Terms of Service</FormLabel>
+          <FormControlLabel
+            control={<Field type='checkbox' name='tos' value={values.tos} component={Checkbox} />}
+            label='I agree'
+          />
+        </FormControl>
 
-      <div>
-        {touched.password && errors.password && <p>{errors.password}</p>}
-        <Field type='password' name='password' placeholder='Password' />
-      </div>
-
-      <label>
-        <Field type='checkbox' name='tos' checked={values.tos} />
-        Accept TOS
-      </label>
-
-      <button disabled={isSubmitting}>Submit</button>
-    </Formik>
+        <Button className='Submit' type='submit' disabled={isSubmitting}>
+          Submit
+        </Button>
+      </Formik>
+    </Paper>
   );
 }
 
@@ -59,6 +78,7 @@ export const Form = withFormik({
     password: Yup.string()
       .min(8, 'Password must be 8 characters or longer')
       .required('Password is required'),
+    tos: Yup.boolean().oneOf([true], 'You are required to agree'),
   }),
   handleSubmit(user, { resetForm, setSubmitting }) {
     UserService.addUser(user as User)
